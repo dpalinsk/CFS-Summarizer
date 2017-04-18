78910299094fb6cb1c64f0aeb3ca2492e2dfc81b
Leo <Leo@ip-87-11.wireless.oberlin.edu>
2017-04-18 16:27:24 -0400

    Got preliminary tf-idf summary working

--- main.py
+++ main.py
@@ -1,8 +1,24 @@
+# An automatic summarizer.
+# Kirsten Vail, Derek Palinski, and Lee Schumann
+
 from nltk.corpus import reuters
 from nltk.corpus import stopwords as swCorpus
 import math
 
-#count how many documents the word appears in
+'''
+A function to sort a dictionary.
+Author: Derek Palinski
+'''
+def sortDict(weights):
+    ret = [(weights[key], key) for key in weights]
+    ret.sort()
+    ret.reverse()
+    return ret
+
+def getPosition(tup):
+    return tup[1]
+
+# count how many documents the word appears in
 docCounts = {}
 for fid in reuters.fileids():
     docWords = set()
@@ -19,27 +35,53 @@ for fid in reuters.fileids():
 swList = swCorpus.words('english')
 stopwords = set(swList)
 
-#create a count for all the words in the article
+# create a count for all the words in the article
 inCount = {}
 for word in reuters.words('test/14862'):
     word = word.lower()  # generalize by making them all lowercase
-    if word not in stopwords:
+    if word not in stopwords and word.isalpha():
         if word in inCount.keys():
             inCount[word] += 1
         else:
             inCount[word] = 1
 
-#Create a weighting for each word in the article
+# Create a weighting for each word in the article
 weights = {}
 size = len(reuters.fileids())
 for word in inCount.keys():
-    weights[word] = inCount[word] * math.log(size/docCounts[word])
+    weights[word] = inCount[word] * math.log(size / docCounts[word])
 
-#print the words in the article with their weights
-for word in weights.keys():
-    print(word + ' ' + str(weights[word]))
+# print the words in the article with their weights
+for tup in sortDict(weights):
+    print(tup[1] + ' ' + str(tup[0]))
 
-#TODO
-    # add up weights to create weighting for sentences
-    # select certain sentences for the summary
-    # See what happens!
+#get sentence weights and keep the top 20 ones.
+length = 10
+impSents = []
+for index, sentence in enumerate(reuters.sents('test/14862')):
+    sentWeight = 0
+    for word in sentence:
+        word = word.lower()
+        if word in weights.keys():
+            sentWeight += weights[word]
+
+    if len(impSents) < length or sentWeight > impSents[length-1][0]:
+        tup = (sentWeight, index, sentence)
+        impSents.append(tup)
+        impSents.sort()
+        if len(impSents) > length:
+            impSents = impSents[:(length-1)]
+print()
+print(reuters.raw('test/14862'))
+print()
+impSents.sort(key=getPosition)
+for tup in impSents:
+    sentence = tup[2]
+    for word in sentence:
+        print(word, end=' ')
+    print()
+
+# TODO
+# add up weights to create weighting for sentences
+# select certain sentences for the summary
+# See what happens!
