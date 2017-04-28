777dffcaa6163bca3f302713fe94239414ac44f3
schumanns <lschuman@oberlin.edu>
2017-04-28 11:36:36 -0400

    modified to accomidate more heuristics

--- summarizer.py
+++ summarizer.py
@@ -9,7 +9,6 @@ import math
 A class for our summarizer object
 '''
 
-
 class Summarizer:
 
     def __init__(self):
@@ -22,7 +21,6 @@ class Summarizer:
     '''
     A function to count how many times various words appear.
     '''
-
     def getDocCounts(self):
         docCounts = {}
         for fid in reuters.fileids():
@@ -38,27 +36,48 @@ class Summarizer:
         return docCounts
 
     '''
-    Summarize and print the text
+    Summarize the text and return the sentences in the form of a list of lists of words
     '''
-
     def summarize(self, article, length):
-        # get the weights for each word in the focus document and print them
-        weights = self.getWordWeights(article)
-        #self.printWeights(weights)
-
         # get the important sentences and print them
-        impSents = self.getSentences(weights, article, length)
-        sentList = self.getList(impSents)
+        important_sentences = self.getSentences(article, length)
+        sentence_list = self.getList(important_sentences)
 
         #print()
         #self.printSents(impSents)
 
-        return sentList
+        return sentence_list
+
+    '''
+    A function which returns the sentences of sufficient weight for a given length in the order of their appearance.
+    '''
+    def getSentences(self, article, length):
+        # get the weights for each word in the focus document and print them
+        word_weights = self.getWordWeights(article)
+        # self.printWeights(weights)
+
+        important_sentences = []
+        for index, sentence in enumerate(reuters.sents(article)):
+            # calculate the weight of the sentence
+            #The heuristics:
+            sum_word_weight = self.getSumofWordWeight(sentence, word_weights)
+            #Add them together with multipliers:
+            sentence_weight = 1*sum_word_weight
+
+            # add the sentence if it is above the least weighted sentence in the list
+            if len(important_sentences) < length or sentence_weight > important_sentences[length - 1][0]:
+                tup = (sentence_weight, index, sentence)
+                important_sentences.append(tup)
+                important_sentences.sort(reverse=True)
+                if len(important_sentences) > length:
+                    important_sentences = important_sentences[:(length - 1)]
+
+        important_sentences.sort(key=self.getPosition)
+        return important_sentences
 
     '''
     Get the weight of each unique word, based on a simple tf-idf scheme.
     '''
-
     def getWordWeights(self, article):
         # get the counts of all the the number of times each word appears within the article
         inCount = self.getWordCounts(article)
@@ -75,7 +94,6 @@ class Summarizer:
     A function to count the number of times each non-stop word in the article appears
     within that article.
     '''
-
     def getWordCounts(self, article):
         # create a count for all the words in the article
         inCount = {}
@@ -91,7 +109,6 @@ class Summarizer:
     '''
     A function which prints the weights of all the words in the focus document in decending order
     '''
-
     def printWeights(self, weights):
         for tup in self.sortDict(weights):
             print(tup[1] + ' ' + str(tup[0]))
@@ -108,34 +125,19 @@ class Summarizer:
         return ret
 
     '''
-    A function which returns the sentences of sufficient weight for a given length in the order of their appearance.
+    A function which gets the sum of the word weights in the sentence
     '''
-
-    def getSentences(self, weights, article, length):
-        impSents = []
-        for index, sentence in enumerate(reuters.sents(article)):
-            # calculate the weight of the sentence
-            sentWeight = 0
-            for word in sentence:
-                word = word.lower()
-                if word in weights.keys():
-                    sentWeight += weights[word]
-
-            # add the sentence if it is above the least weighted sentence in the list
-            if len(impSents) < length or sentWeight > impSents[length - 1][0]:
-                tup = (sentWeight, index, sentence)
-                impSents.append(tup)
-                impSents.sort(reverse=True)
-                if len(impSents) > length:
-                    impSents = impSents[:(length - 1)]
-
-        impSents.sort(key=self.getPosition)
-        return impSents
+    def getSumofWordWeight(self, sentence, word_weights):
+        sum_word_weight = 0
+        for word in sentence:
+            word = word.lower()
+            if word in word_weights.keys():
+                sum_word_weight += word_weights[word]
+        return sum_word_weight
 
     '''
     A function which serves as the key to sorting the final selected sentences.
     '''
-
     def getPosition(self, tup):
         return tup[1]
 
