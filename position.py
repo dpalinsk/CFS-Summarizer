1745d4754b939858aa3293c983266907c5407deb
Kirsten Vail <kvail@oberlin.edu>
2017-04-29 14:08:34 -0400

    updated position function, takes in list of tokenized pars

--- position.py
+++ position.py
@@ -2,34 +2,21 @@
 import nltk
 import nltk.data
 
-def main():
-	#the following code is to deal with opening/reading text files can be deleted later on
-	text=open("text.txt", 'r')
-	lines= text.readlines()
-	#this separates the file into paragraphs and puts it into the array pars. 
-	pars=[]
-	for line in lines:
-		if "\n" in line:
-			if line!="\n":
-				pars.append(line)
 
-	#this opens the english tokenizer from nltk
-	tokenizer = nltk.data.load('tokenizers/punkt/english.pickle')
-	
-	#resets the file to the beginning so the tokenizer can use it.
-	text.seek(0)
-	whole = text.read()
 
-	#all<=tuple array of format (weight, sentence)
+	#position takes in pars and returns a list of tuples of (weight, sentence)
+	#pars is an list of pargraphs in the form of a list of tokenized sentences
+def position(pars):
+
+	#this will be the list in which we store final weights
 	all=[]
-	
+
 	#find number of paragraphs and median paragraph
 	sizep = len(pars)
 	sizep=sizep-1
 	medP=sizep/2
+	
 
-	print("median par number= ", medP)
-	#for each paragraph in file find the median line indice and then define pweight (paragraph weight) based of that
 	for x in range(0,len(pars)):
 
 		size1=len(pars)
@@ -45,10 +32,8 @@ def main():
 			pweight=pweight/size1
 			pweight=1-pweight
 
-		print(medP)
-		print("PAR START-------------  ", pweight, ", ", x)		
-		#tokenize the sentences in paragraph, then find median (-1 because 0 indexed)
-		liste= tokenizer.tokenize(pars[x])
+		#take the tokenized paragraph (pars[x]) then find median (-1 because 0 indexed)
+		liste= pars[x]
 		numS= float(len(liste)-1)
 		median= float(numS/2)
 
@@ -80,12 +65,12 @@ def main():
 					sweight=test
 					weight=0.5*(sweight + pweight)
 				all.append( (weight, elt))
-			print("weight, sweight, pweight, elt:   ", weight, sweight, pweight, elt)
-
-
+	return all
 
 	
-	
+def main():
+	pars=[ ["this is a sentence.","here is also a sentence."], ["a yes a sentence for par 2.", "we are in par 2.", "what a dream is par 2"] ]
+	print(position(pars)) 	
 	
 
 main()
